7688ec8cc6d158c8232f0eeaf49090c63fc03486
lirl <lironglian@ewell.cc>
2020-08-23 16:49:36 +0800

    loader的使用

---
+++ lesson2/src/index.js
@@ -0,0 +1,37 @@
+import pic from './assets/images/logo.png'
+// import './assets/css/index.css'
+import './assets/css/index.less'
+console.log(pic);
+var img = new Image()
+img.src = pic
+var root = document.getElementById('app')
+root.append(img)
+
+// 前端开发环境
+// 这个项目是什么类型的项目，目标环境是什么？
+// 是spa 移动端 mpa pc端？
+// 这个项目的团队 2+ 制定代码规范，提交规范
+// html css assests img font
+// js =>es6+ 模块化。vue/ react 等技术栈
+
+// 提升本地开发体验的服务
+// -> 快速定位错误信息
+// -> 本地mock数据，解决跨域问题
+// -> 自动打开浏览器窗口，代码修改后刷新页面
+
+// 线上发布
+// -> 代码压缩，资源压缩
+// -> css html js 图片
+
+// 数据收集
+// 前端错误收集 报警
+// 前端性能监控
+// 环境数据
+// -> 用户分布 通过用户的访问ip pv/uv
+// -> 浏览器头部信息：用户的使用群体，手机多一些，还是pc多一些，什么型号，最终得出需要兼容哪些浏览器，什么版本，手机浏览器的型号
+// -> 用户的行为收集：得到用户的停留时间，点击过程，得出用户信息的热力图（可以很简单的得出哪些广告位，热门业务）
+
+
+
+// 图片处理
+
---
+++ lesson2/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const htmlwebpackplugin = require('html-webpack-plugin')
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /.(png|jpe?g|gif)$/, // svg有专门的的loader处理
+        use: {
+          // loader: 'file-loader',
+          loader: 'url-loader', // url-loader 依赖file-loader，有file-loader的所有功能
+          options: {
+            // name: "assets/images/[name].[ext]" // 加上路径容易出现路径对不上问题
+            name: "[name].[ext]",
+            outputPath: 'assets/images/',
+            limit: 3 * 1024 // 处理图片转换，大于3kb直接输出独立图片文件，小于3kb将会被转成base64格式放到bundle里面
+          }
+        }
+      },
+      {
+        test: /.(eot|woff)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: "[name].[ext]",
+          }
+        }
+      },
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      }
+    ]
+  },
+  plugins: [
+    new htmlwebpackplugin({
+      template: "./src/assets/html/index.html",
+      filename: "index.html"
+    })
+  ]
+}
